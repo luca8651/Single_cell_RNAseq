3218d0f17a6a054f4d87b3598b3ed61294fa9804
Luca Penso Dolfin <luca8650@libero.it>
2020-04-24 09:36:18 +0200

    Update Annotate_integrate_3species_SCT_single_samples_correct_only_Integration_rPCA.r

--- Annotate_integrate_3species_SCT_single_samples_correct_only_Integration_rPCA.r
+++ Annotate_integrate_3species_SCT_single_samples_correct_only_Integration_rPCA.r
@@ -1,3 +1,4 @@
+#load required packages
 library(Seurat)
 library(future)
 options(future.globals.maxSize=40000 * 1024^2)
@@ -14,14 +15,7 @@ library(batchelor)
 library(scater)
 
 load("Annotate_integrate_3species_SCT_single_samples_correct.RData") 
-rm(seu_3species)  
-rm(seu_hsa)  
-rm(seu_mmu)  
-rm(seu_nmr)  
-rm(seu3)
-rm(nmr.data)
 
-###DOES NOT WORK WITHOUT SCALE DATA
 
 for (i in 1:length(seu_list)) {
 DefaultAssay(seu_list[[1]])="RNA"
@@ -35,26 +29,17 @@ seu_list <- lapply(X = seu_list, FUN = function(x) {
 
 features <- SelectIntegrationFeatures(object.list = seu_list)
 
-#    x <- ScaleData(x, features = features, verbose = TRUE)
-#    x <- RunPCA(x, features = features, verbose = FALSE)
-
 seu_list <- lapply(X = seu_list, FUN = function(x) {
     x <- ScaleData(x,features = features,  verbose = TRUE)
     x <- RunPCA(x, features = features, verbose = FALSE)
 })
 
-#for (i in 1:length(seu_list)) {
-#DefaultAssay(seu_list[[1]])="RNA"
-#seu_list[[i]]<- NormalizeData(seu_list[[i]], verbose = FALSE)
-#seu_list[[i]]<- FindVariableFeatures(seu_list[[i]], verbose = FALSE)
-#seu_list[[i]] <- ScaleData(seu_list[[i]], features = features, verbose = FALSE)
-#seu_list[[i]] <- RunPCA(seu_list[[i]], features = features, verbose = FALSE)
-#}
 
+#find anchors
 anchors <- FindIntegrationAnchors(object.list = seu_list,reference=c(1,6,15,22,31), reduction = "rpca", dims = 1:30)
 
 saveRDS(anchors,file = "/icgc/dkfzlsdf/analysis/B210/Luca/Rstudio/SCT_singleSamples/Annotate_integrate_3species_SCT_single_samples_correct_RpcaAnchors.rds")
-#seu_3species_new <- IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE)
+#perform integration at the biopsy level:
 seu_3species_new <- IntegrateData(anchorset = anchors,  verbose = TRUE)
 saveRDS(seu_3species_new,file = "/icgc/dkfzlsdf/analysis/B210/Luca/Rstudio/SCT_singleSamples/Annotate_integrate_3species_SCT_single_samples_correct_seuObject_bsub_rPCA.rds" )
 
